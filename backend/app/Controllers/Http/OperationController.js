e7e0b5e2ad3000ed27053dec30f7a44e618dd24d
Mateus Bezerra <mateusbzerra@gmail.com>
2019-11-02 02:19:03 -0300

    feat: Returning operations by year

--- backend/app/Controllers/Http/OperationController.js
+++ backend/app/Controllers/Http/OperationController.js
@@ -10,24 +10,38 @@ class OperationController {
   async index({ request, response, params }) {
     const query = request.all();
     let month = Number(new Date().getMonth() + 1);
+    let year = Number(new Date().getFullYear());
     if (query.month) {
       month = Number(query.month);
     }
+    if (query.year) {
+      year = Number(query.year);
+    }
     const operations = await Operation.query()
-      .whereRaw('EXTRACT(MONTH FROM date) = ?', [month])
+      .whereRaw(
+        'EXTRACT(MONTH FROM date) = ? AND EXTRACT(YEAR FROM date) = ?',
+        [month, year]
+      )
       .fetch();
     const [{ incoming }] = await Operation.query()
-      .whereRaw('EXTRACT(MONTH FROM date) = ?', [month])
+      .whereRaw(
+        'EXTRACT(MONTH FROM date) = ? AND EXTRACT(YEAR FROM date) = ?',
+        [month, year]
+      )
       .andWhere({ incoming: true })
       .sum('value as incoming');
     const [{ outcoming }] = await Operation.query()
-      .whereRaw('EXTRACT(MONTH FROM date) = ?', [month])
+      .whereRaw(
+        'EXTRACT(MONTH FROM date) = ? AND EXTRACT(YEAR FROM date) = ?',
+        [month, year]
+      )
       .andWhere({ incoming: false })
       .sum('value as outcoming');
 
     return {
       operations,
       month,
+      year,
       balance: {
         incoming: Number(incoming),
         outcoming: Number(outcoming)
@@ -49,6 +63,7 @@ class OperationController {
     }
     newValue = newValue * 1000;
     data.value = newValue;
+
     const operation = await Operation.create(data);
     return operation;
   }
