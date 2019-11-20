0a55242863aca00cd6e9528b31be76fb031b8d16
Mateus Bezerra <mateusbzerra@gmail.com>
2019-11-20 19:51:12 -0300

    refactor: api keys added

--- config/keys.js
+++ config/keys.js
@@ -1,1 +1,5 @@
-module.exports = ['A?D(G-KaPdSgVkYp3s6v9y$B&E)H@MbQ', '123'];
+module.exports = [
+  "A?D(G-KaPdSgVkYp3s6v9y$B&E)H@MbQ",
+  "VpNLqSXU6M5BJSkDkdg7GnYZ4Ks83Y6jG",
+  "UkxuRMXnV6t7AKBWra6qn7U9HN4cc5GM6"
+];
