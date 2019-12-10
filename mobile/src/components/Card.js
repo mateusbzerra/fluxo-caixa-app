c082037936aca8b78141e074cd2258bca2224a16
Mateus Bezerra <mateusbzerra@gmail.com>
2019-12-10 14:24:19 -0300

    style: created_at added to Card component

--- mobile/src/components/Card.js
+++ mobile/src/components/Card.js
@@ -1,25 +1,34 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
-
+import moment from 'moment';
 // import { Container } from './styles';
 
 export default function Card({ data }) {
+  function formatDate(date) {
+    return moment(date).format('DD/MM');
+  }
   return (
     <View style={styles.container}>
-      <View style={styles.content}>
-        <Text style={styles.description}>{data.description}</Text>
-        <Text style={[styles.value, data.incoming && styles.greenColor]}>
-          {data.formated_value}
-        </Text>
-      </View>
-      <View style={styles.typeContainer}>
-        {data.type === 2 || data.type === 3 ? (
-          <Icon name="credit-card" color="#555" size={15}></Icon>
-        ) : (
-          data.type === 1 && <Icon name="money" color="#555" size={15}></Icon>
-        )}
-        <Text style={styles.type}>{data.type_name}</Text>
+      <Text style={styles.date}>{formatDate(data.created_at)}</Text>
+      <View style={styles.rightContent}>
+        <View>
+          <View style={styles.content}>
+            <Text style={styles.description}>{data.description}</Text>
+            <Text style={[styles.value, data.incoming && styles.greenColor]}>
+              {data.formated_value}
+            </Text>
+          </View>
+        </View>
+
+        <View style={styles.typeContainer}>
+          {data.type === 2 || data.type === 3 ? (
+            <Icon name="credit-card" color="#555" size={15}></Icon>
+          ) : (
+            data.type === 1 && <Icon name="money" color="#555" size={15}></Icon>
+          )}
+          <Text style={styles.type}>{data.type_name}</Text>
+        </View>
       </View>
     </View>
   );
@@ -39,7 +48,9 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.3,
     shadowRadius: 3,
     elevation: 5,
-    borderRadius: 20
+    borderRadius: 20,
+    flexDirection: 'row',
+    alignItems: 'center'
   },
   greenColor: {
     color: '#3CB371'
@@ -52,8 +63,22 @@ const styles = StyleSheet.create({
   description: {
     color: '#222'
   },
+  rightContent: {
+    flex: 1,
+    flexDirection: 'column',
+    borderLeftWidth: 0.2,
+    borderLeftColor: '#888',
+    paddingLeft: 10
+  },
+  date: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: '#888',
+    paddingRight: 10
+  },
   content: {
     alignItems: 'center',
+    flex: 1,
     justifyContent: 'space-between',
     flexDirection: 'row'
   },
